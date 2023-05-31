ebdbbc7ac1f15db011b9954076f532b9fca080f7
Abhijeet Singh <89379140+abhijeet3001@users.noreply.github.com>
2023-05-31 16:58:01 +0530

    Refactor code for improved performance and readability

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function Book(title, author, pages, isRead) {
 }
 
 Book.prototype.toggleRead = function () {
-  this.isRead = this.isRead === "true" ? "false" : "true";
+  this.isRead = !this.isRead;
   return this.isRead;
 };
 
@@ -28,33 +28,31 @@ function addBookToLibrary(title, author, pages, isRead) {
 
 function displayBook(book) {
   let bookCard = document.createElement("div");
+  bookCard.classList.add("book-card");
+  bookCard.setAttribute("data-book-no", bookCount);
 
   const title = makeChildElement("p", book.title);
   const author = makeChildElement("p", book.author);
   const pages = makeChildElement("p", book.pages);
-  const isRead = makeChildElement("p", book.isRead === "true" ? "Yes" : "No");
-  isRead.classList.add("read");
-
   const removeButton = makeChildElement("button", "Remove Book");
-  removeButton.classList.add("btn-remove");
-  removeButton.addEventListener("click", removeBook);
+  const toggleComplete = makeChildElement(
+    "button",
+    (book.isRead ? " " : "Not") + " Completed"
+  );
 
-  let readMessage = (book.isRead === "true" ? " " : "Not") + " Completed";
-  const toggleComplete = makeChildElement("button", readMessage);
+  removeButton.classList.add("btn-remove");
   toggleComplete.classList.add("btn-read");
-  toggleComplete.style.backgroundColor =
-    book.isRead === "true" ? "green" : "red";
-  toggleComplete.addEventListener("click", toggleCompleteStatus);
+  toggleComplete.style.backgroundColor = book.isRead ? "green" : "red";
+
+  removeButton.onclick = removeBook;
+  toggleComplete.onclick = toggleCompleteStatus;
 
   bookCard.appendChild(title);
   bookCard.appendChild(author);
   bookCard.appendChild(pages);
-  bookCard.appendChild(isRead);
   bookCard.appendChild(toggleComplete);
   bookCard.appendChild(removeButton);
 
-  bookCard.classList.add("book-card");
-  bookCard.setAttribute("data-book-no", bookCount);
   bookContainer.insertBefore(bookCard, addBookForm);
 }
 
@@ -76,7 +74,6 @@ function updateAttributes(index) {
 
 function removeBook(e) {
   index = e.target.parentElement.getAttribute("data-book-no");
-  console.log(e.target);
   myLibrary.splice(index, 1);
   bookCount--;
   let bookCardToRemove = document.querySelector(`[data-book-no = "${index}"]`);
@@ -87,14 +84,8 @@ function removeBook(e) {
 function toggleCompleteStatus(e) {
   index = e.target.parentElement.getAttribute("data-book-no");
   const currentReadStatus = myLibrary[index - 1].toggleRead();
-  const currentReadElement = document.querySelector(
-    `[data-book-no ="${index}"] .read`
-  );
-  e.target.style.backgroundColor =
-    currentReadStatus === "true" ? "green" : "red";
-  e.target.textContent =
-    (currentReadStatus === "true" ? "" : "Not") + " Completed";
-  currentReadElement.textContent = currentReadStatus === "true" ? "Yes" : "No";
+  e.target.style.backgroundColor = currentReadStatus ? "green" : "red";
+  e.target.textContent = (currentReadStatus ? "" : "Not") + " Completed";
 }
 function toggleFormVisibility() {
   addBookForm.classList.toggle("hide");
@@ -105,7 +96,7 @@ addBookForm.addEventListener("submit", (e) => {
   const title = document.getElementById("title");
   const author = document.getElementById("author");
   const pages = document.getElementById("pages");
-  const read = document.getElementById("isComplete").value === "true";
+  const read = document.getElementById("isComplete").checked;
   addBookToLibrary(title.value, author.value, pages.value, read);
   toggleFormVisibility();
   e.preventDefault();
@@ -116,10 +107,8 @@ function addBooksToDisplay() {
   myLibrary.forEach((book) => displayBook(book));
 }
 
-removeBookButton.forEach((btn) => btn.addEventListener("click", removeBook));
-
-addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, "true");
-addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, "false");
-addBookToLibrary("1984", "George Orwell", 328, "true");
-addBookToLibrary("Pride and Prejudice", "Jane Austen", 432, "true");
-addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 224, "false");
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, false);
+addBookToLibrary("1984", "George Orwell", 328, true);
+addBookToLibrary("Pride and Prejudice", "Jane Austen", 432, true);
+addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 224, false);
